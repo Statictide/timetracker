a60c299dbdc11700d18496fc2651fc548df3f8cc
Mark Jensen <marknjensen@outlook.com>
2024-01-16 02:44:42 +0100

    finished tutorial

---
+++ README.md
@@ -0,0 +1,17 @@
+# run server
+
+Watches the assets and runs tailwind tree shaking
+
+```
+pnpm dlx tailwindcss -i styles/tailwind.css -o assets/main.css --watch
+```
+
+```
+pnpm format
+```
+
+Run server
+
+```
+cargo run
+```
--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,88 @@
-use axum::{
-    routing::get,
-    Router,
-};
+mod templates;
+
+use std::sync::{Arc, Mutex};
+
+use axum::{response::IntoResponse, routing::{get, post}, Router, extract::State, Form};
 use tracing::info;
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+use tower_http::services::ServeDir;
+use serde::Deserialize;
+
+struct AppState {
+    todos: Mutex<Vec<String>>,
+}
 
 #[tokio::main]
 async fn main() {
-    tracing_subscriber::registry()
-    .with(
-        tracing_subscriber::EnvFilter::try_from_default_env()
-            .unwrap_or_else(|_| "with_axum_htmx_askama=debug".into()),
-    )
-    .with(tracing_subscriber::fmt::layer())
-    .init();
+    let subscriber = tracing_subscriber::FmtSubscriber::new();
+    tracing::subscriber::set_global_default(subscriber)
+        .expect("There should only be one global subscriber");
+
+    let app_state = Arc::new(AppState {
+        todos: Mutex::new(vec![]),
+    });
 
     info!("initializing router...");
 
-    let app = Router::new().route("/", get(index));
+    let assets_path = std::env::current_dir().unwrap();
+    let assets_path = assets_path.to_str().unwrap();
+    
+    let api_router = Router::new()
+        .route("/hello", get(hello_from_the_server))
+        .route("/todos", post(add_todo))
+        .with_state(app_state);
+
+    let app = Router::new()
+        .nest("/api", api_router)
+        .route("/", get(index))
+        .route("/another-page", get(another_page))
+        .nest_service("/assets", ServeDir::new(format!("{assets_path}/assets")));
+
     let port = 3000;
-    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{port}")).await.unwrap();
-    
-    let addr = std::net::SocketAddr::from(([0, 0, 0, 0], port));
-    info!("router initialized, now listening on port {}", port);
-    
-    axum::serve(listener, app).await.unwrap();
+    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{port}"))
+        .await.unwrap();
+
+    let server = axum::serve(listener, app);
+    info!("Server running at http://127.0.0.1:{port}");
+    server.await.unwrap();
 }
 
-async fn index() -> String {
+async fn index() -> impl IntoResponse {
+    use templates::*;
+
     info!("Request");
-    return "Hello world!".into();    
+    let template = HelloTemplate {};
+    HtmlTemplate(template)
+}
+
+async fn another_page() -> impl IntoResponse {
+    use templates::*;
+
+    let template = AnotherPageTemplate {};
+    HtmlTemplate(template)
+}
+
+
+async fn hello_from_the_server() -> &'static str {
+    "Hello!"
+}
+
+#[derive(Deserialize)]
+struct TodoRequest {
+    pub todo: String
+}
+
+async fn add_todo(
+    State(state): State<Arc<AppState>>,
+    Form(todo): Form<TodoRequest>,
+) -> impl IntoResponse {
+    use templates::*;
+    
+    let mut lock = state.todos.lock().unwrap();
+    lock.push(todo.todo);
+
+    let template = TodoList {
+        todos: lock.clone(),
+    };
+
+    HtmlTemplate(template)
 }
